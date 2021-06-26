fb3ac59bf75e48bd617bac550f339134551a103e
mithun shanbhag <mithunshanbhag@hotmail.com>
2021-06-27 00:32:04 +0530

    placeholder routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+
+const appRoutes: Routes = [
+  // routes should be ordered from most specific to least specific.
+  // { path: 'authcallback', component: AuthenticationCallbackComponent },
+  // { path: 'checkoutsessioncallback/:id', component: CheckoutSessionCallbackComponent },
+  // { path: 'editor/:id', component: DiagramEditorComponent, canActivate: [AuthGuard], canDeactivate: [DiagramEditorDeactivateGuard], },
+  // { path: 'logout', component: LogoutComponent, },
+  // { path: 'viewer/:id', component: DiagramViewerComponent, },
+  // { path: '', component: DiagramEditorComponent, canActivate: [AuthGuard], /*resolve: {userProfileData: UserProfileResolverService}*/ }, // default route
+  { path: '**', redirectTo: '/', }, // wildcard route
+];
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
