c4341b1de0bffd159a492c3b398b6a88665d12a2
mithun shanbhag <shanbhag.mithun@gmail.com>
2021-07-08 19:52:24 +0530

    placeholder html + css for all sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
-import { CustomerTestimonialsComponent } from './components/customer-testimonials/customer-testimonials.component';
 import { FeaturesComponent } from './components/features/features.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { PricingTablesComponent } from './components/pricing-tables/pricing-tables.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 
 @NgModule({
@@ -20,7 +20,7 @@ import { PricingTablesComponent } from './components/pricing-tables/pricing-tabl
     FeaturesComponent,
     PricingTablesComponent,
     AboutComponent,
-    CustomerTestimonialsComponent
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
