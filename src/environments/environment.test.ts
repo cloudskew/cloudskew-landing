6bd244d70896e465b0cadde7e007c67104cde9e2
mithun shanbhag <mithunshanbhag@hotmail.com>
2021-07-11 00:58:35 +0530

    fix appUrl for test env

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  appUrl: 'https://cloudskewlandingtest.azureedge.net',
+  appUrl: 'https://cloudskewuitest.azureedge.net',
   blogUrl: 'https://cloudskewblogtest.azureedge.net',
   cdnUrlPrefix: 'https://cloudskewtest.azureedge.net',
   docsUrl: 'https://cloudskewdocstest.azureedge.net',
