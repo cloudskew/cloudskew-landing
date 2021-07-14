78942a2ef8ffac81239fcc3356ce9b07583983ab
mithun shanbhag <mithunshanbhag@hotmail.com>
2021-07-14 22:50:21 +0530

    Added typedjs support

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UrlConstants } from 'src/app/constants/urlConstants';
 import { environment } from 'src/environments/environment';
+import Typed from 'typed.js';
+
 
 @Component({
   selector: 'app-hero',
@@ -16,5 +18,29 @@ export class HeroComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var typedjsOptions = {
+      strings: [
+        'AWS Diagrams',
+        'Azure Diagrams',
+        'GCP Diagrams',
+        'Kubernetes Diagrams',
+        'Oracle Cloud Diagrams',
+        'Alibaba Cloud Diagrams',
+        'Elastic Cloud',
+        'Flowcharts',
+        'Anything',
+        'Absolutely Anything',
+      ],
+      typeSpeed: 100,
+      startDelay: 5000,
+      backSpeed: 30,
+      backDelay: 2000,
+      loop: true,
+      loopCount: Infinity,
+      showCursor: false,
+    };
+
+    var typedjs = new Typed('.cs-section-title-typedjs', typedjsOptions);
   }
 }
