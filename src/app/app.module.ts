6fd5b02bd9534ba7a0aabe16156e32a0e95e682c
Mithun Shanbhag <mithunshanbhag@users.noreply.github.com>
2022-01-28 21:02:20 +0530

    More solution pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { FeaturesComponent } from './components/features/features.component';
@@ -12,6 +13,14 @@ import { MobileMenuComponent } from './components/mobile-menu/mobile-menu.compon
 import { PricingComponent } from './components/pricing/pricing.component';
 import { SamplesComponent } from './components/samples/samples.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+
+// Note: If you modify any routes here, please be sure to update the 'routes.txt', 'sitemap.txt' files accordingly.
+// Context: https://angular.io/guide/prerendering#prerendering-dynamic-routes
+const routes: Routes = [
+  { path: 'solutions/:solutionId', component: HeroComponent, },
+  { path: '', component: HeroComponent, }, // default route
+  { path: '**', redirectTo: '/', }, // wildcard route
+];
 
 @NgModule({
   declarations: [
@@ -30,6 +39,11 @@ import { TestimonialsComponent } from './components/testimonials/testimonials.co
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
     MatSidenavModule,
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+      scrollOffset: [0, 64]
+    } as ExtraOptions),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,8 +1,13 @@
 import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 import { Configuration, init as typewriterInit } from 'ityped';
+import { PageTitleConstants } from 'src/app/constants/page-title-constants';
+import { SolutionKeys } from 'src/app/constants/solution-keys';
+import { SolutionRoutes } from 'src/app/constants/solution-routes';
 import { UrlConstants } from 'src/app/constants/urlConstants';
-import { environment } from 'src/environments/environment';
+import { PageCustomization } from './page-customization';
 
 
 @Component({
@@ -14,20 +19,77 @@ export class HeroComponent implements OnInit {
 
   //
   appUrl = UrlConstants.appUrl;
-  heroImageUrl = `${environment.cdnUrlPrefix}/assets/misc/landing-page-hero-3.png`;
+  heroImageUrl = UrlConstants.heroImageUrlAzure;
+  customizations: Record<SolutionKeys, PageCustomization> = {
+    ALIBABA: { pageTitle: PageTitleConstants.ALIBABA, heroImageUrl: UrlConstants.heroImageUrlAlibaba },
+    AWS: { pageTitle: PageTitleConstants.AWS, heroImageUrl: UrlConstants.heroImageUrlAws },
+    AZURE: { pageTitle: PageTitleConstants.AZURE, heroImageUrl: UrlConstants.heroImageUrlAzure },
+    CLOUD: { pageTitle: PageTitleConstants.CLOUD, heroImageUrl: UrlConstants.heroImageUrlAzure },
+    DEFAULT: { pageTitle: PageTitleConstants.DEFAULT, heroImageUrl: UrlConstants.heroImageUrlAzure },
+    GCP: { pageTitle: PageTitleConstants.GCP, heroImageUrl: UrlConstants.heroImageUrlGCP },
+    K8S: { pageTitle: PageTitleConstants.K8S, heroImageUrl: UrlConstants.heroImageUrlK8S },
+    MULTICLOUD: { pageTitle: PageTitleConstants.MULTICLOUD, heroImageUrl: UrlConstants.heroImageUrlAzure },
+    OCI: { pageTitle: PageTitleConstants.OCI, heroImageUrl: UrlConstants.heroImageUrlOCI },
+  };
 
   constructor(
+    private route: ActivatedRoute,
+    private titleService: Title,
     // details: https://angular.io/guide/universal#working-around-the-browser-apis
     @Inject(DOCUMENT) private document: HTMLDocument,
     @Inject(PLATFORM_ID) private platformId: Object,
   ) { }
 
+  //#region lifecycle hooks
 
   ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.enableTypewriter();
-    }
+    this.route.paramMap.subscribe(params => {
+      if (isPlatformBrowser(this.platformId)) {
+        this.enableTypewriter();
+      }
+
+      const solutionId = params.get('solutionId');
+      const customizations = this.getPageCustomization(solutionId);
+
+      this.titleService.setTitle(customizations.pageTitle);
+      this.heroImageUrl = customizations.heroImageUrl;
+    });
   }
+
+  //#endregion
+
+  //#region private helper methods
+
+  private getPageCustomization(solutionId: string | null): PageCustomization {
+    if (solutionId) {
+      switch (solutionId.trim().toLowerCase()) {
+        case SolutionRoutes.ALIBABA1:
+          return this.customizations['ALIBABA'];
+        case SolutionRoutes.AWS1:
+          return this.customizations['AWS'];
+        case SolutionRoutes.AZURE1:
+          return this.customizations['AZURE'];
+        case SolutionRoutes.CLOUD1:
+          return this.customizations['CLOUD'];
+        case SolutionRoutes.GCP1:
+        case SolutionRoutes.GCP2:
+          return this.customizations['GCP'];
+        case SolutionRoutes.K8S1:
+          return this.customizations['K8S'];
+        case SolutionRoutes.MULTICLOUD1:
+          return this.customizations['MULTICLOUD'];
+        case SolutionRoutes.OCI1:
+        case SolutionRoutes.OCI2:
+          return this.customizations['OCI'];
+        default:
+        case 'DEFAULT':
+          return this.customizations['DEFAULT'];
+      }
+    }
+
+    return this.customizations['DEFAULT'];
+  }
+
 
   private enableTypewriter() {
     const elem: Element = this.document.querySelector('#cs-section-title-cursor') as Element;
@@ -54,4 +116,6 @@ export class HeroComponent implements OnInit {
 
     typewriterInit(elem, config);
   }
+
+  //#endregion
 }
